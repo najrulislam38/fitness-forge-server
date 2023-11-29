aed78253802066f0eee25c3974b033b9fa6d04e6
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2023-11-29 09:38:06 +0600

    connect with mongodb and create a blogCollection and provide get operation

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// const uri =
+//   "mongodb+srv://<username>:<password>@cluster0.va14gvo.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const blogCollection = client.db("fitnessForgeDB").collection("blogs");
+
+    app.get("/blogs", async (req, res) => {
+      try {
+        const result = await blogCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Fitness Forge server is running");
